b7f63a4dafa608b137441740e9a7daa2fa033c28
Henry Escobar <henrye@gmail.com>
2024-03-09 17:00:23 -0800

    modified frontend route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :photographs, only: [:index]
-
-  get '*path', to: 'frontend#index', constraints: ->(request) do
-  !request.xhr? && request.format.html?
-  end
+  get '*path', to: 'frontend#index'
+  get '/', to: 'frontend#index'
 end
