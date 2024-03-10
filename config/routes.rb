54040d52dd6f9d43fb0e940bdf8dcad33cd19c40
Henry Escobar <henrye@gmail.com>
2024-03-09 16:43:53 -0800

    added controller action to handle index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :photographs, only: [:index]
+
+  get '*path', to: 'frontend#index', constraints: ->(request) do
+  !request.xhr? && request.format.html?
+  end
 end
