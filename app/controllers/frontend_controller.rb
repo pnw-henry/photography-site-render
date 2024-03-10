0786214cbe12d317b328eb0af66415bc5cbe90bb
Henry Escobar <henrye@gmail.com>
2024-03-09 22:26:33 -0800

    frontend controller change

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,5 +1,11 @@
 class FrontendController < ApplicationController
-  def index
-    render file: Rails.root.join('public', 'build', 'index.html'), layout: false
-  end
+    before_action :set_headers, only: [:index]
+    def index
+        render file: Rails.root.join('public', 'build', 'index.html'), layout: false
+    end
+
+    def set_headers
+        if request.path.ends_with?('.js')
+        response.headers["Content-Type"] = "application/javascript"
+    end
 end
