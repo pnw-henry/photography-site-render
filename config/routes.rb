bca1f3b33d544a522599a64efb548615826adf84
Henry Escobar <henrye@gmail.com>
2024-03-10 19:11:41 -0700

    render yaml and render bin changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :photographs, only: [:index]
   get '*path', to: 'frontend#index'
   get '/', to: 'frontend#index'
+
+  # Defines the root path route ("/")
+  root "render#index"
 end
