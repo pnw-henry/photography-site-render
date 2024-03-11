5682d301c0ff42c41f25d87de3507f73a5d7b020
Henry Escobar <henrye@gmail.com>
2024-03-11 00:36:49 -0700

    added file path raise error

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,5 +1,10 @@
 class FrontendController < ApplicationController
   def index
-    send_file Rails.root.join('public', 'build', 'index.html'), type: 'text/html', disposition: 'inline'
+    file_path = Rails.root.join('public', 'build', 'index.html')
+    if File.exist?(file_path)
+      send_file(file_path, type: 'text/html', disposition: 'inline')
+    else
+      raise ActionController::RoutingError, 'Not Found'
+    end
   end
 end
