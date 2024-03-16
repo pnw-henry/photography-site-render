5630cf9dc1e5cf909efcfc5fe2a88715f91fd824
Henry Escobar <henrye@gmail.com>
2024-03-16 00:18:36 -0700

    reverted build and production config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,10 +4,10 @@ set -o errexit
 
 bundle install
 # clean
-rm -rf public/build
+rm -rf public
 # build
 npm install --prefix client && npm run build --prefix client
 # migrate
 bundle exec rake db:migrate
 # postbuild
-cp -a client/build/. public/build/
+cp -a client/build/. public/
